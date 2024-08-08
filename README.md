2dfa808bb20949130edb57bf89481d5f83ba29f7
Abhay Agarwal <Abhay.agarwal@newput.com>
2024-08-08 15:57:04 +0530

    update readme

--- README.md
+++ README.md
@@ -3,4 +3,9 @@ build .env file which contains:
     PORT = 
     DB_URL = 
     JWT_SECRET = 
+
+    EMAIL_HOST=
+    EMAIL_PORT=
+    EMAIL_USERNAME=
+    EMAIL_PASSWORD=
 ```
--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -4,6 +4,8 @@ import { updateUserProfile, addFriend, removeFriend } from '../controllers';
 
 import multer from 'multer';
 import catchAsync from '../util/catchAsync';
+import validateRequest from '../middleware/validationMiddleware';
+import { userUpdateSchema } from '../validation/userValidation';
 
 const upload = multer({ dest: 'uploads/' });
 
@@ -13,7 +15,7 @@ router.get("/", (req, res) => {
     res.send("User page");
 });
 
-router.put('/profile', protect, upload.single('profilePicture'), catchAsync(updateUserProfile));
+router.put('/profile', protect,validateRequest(userUpdateSchema), upload.single('profilePicture'), catchAsync(updateUserProfile));
 
 router.post('/friend', protect, catchAsync(addFriend));
 router.delete('/friend', protect, catchAsync(removeFriend));
